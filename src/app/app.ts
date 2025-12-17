bf001729f98324d3f96426c14a99cae759efdaad
Aryan Chaturvedi <mrarch@Aryans-MacBook-Air.local>
2025-12-17 23:40:46 +0530

    Day 2 - component creation and use of them in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
+import { Header } from './features/dashboard/pages/header/header';
+import { Footer } from './features/dashboard/pages/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Dashboard,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
